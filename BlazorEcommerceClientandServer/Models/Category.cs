964ac1f05b65b8579ac72f8abbeead52a2b3e286
chad solomon <chadcsoloman@gmail.com>
2024-06-07 00:30:31 -0400

    Add project files.

---
+++ BlazorEcommerceClientandServer/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorEcommerceClientandServer.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public string? Icon { get; set; }
+        public List<Product>? Products { get; set; }
+    }
+}
